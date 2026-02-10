0682047ba7f8e6d2d0b14eb0493284da3b9d5f85
anatoly yakovenko <aeyakovenko@gmail.com>
2026-02-10 01:59:27 +0000

    turbine: tighten MCP dispatch cache refresh and function surface

--- turbine/src/broadcast_stage/standard_broadcast_run.rs
+++ turbine/src/broadcast_stage/standard_broadcast_run.rs
@@ -654,6 +654,7 @@ impl StandardBroadcastRun {
         let proposer_indices = {
             let mut cache = self.mcp_leader_schedule_cache.lock().unwrap();
             let cache = cache.get_or_insert_with(|| LeaderScheduleCache::new_from_bank(bank));
+            cache.set_root(bank);
             cache.proposer_indices_at_slot(bank.slot(), &self.identity, Some(bank))
         };
         if proposer_indices.is_empty() {
@@ -696,7 +697,6 @@ impl StandardBroadcastRun {
 
     fn maybe_dispatch_mcp_shreds(
         &self,
-        _shreds: &[Shred],
         batch_info: &Option<BroadcastShredBatchInfo>,
         cluster_info: &ClusterInfo,
         bank_forks: &RwLock<BankForks>,
@@ -925,7 +925,6 @@ impl BroadcastRun for StandardBroadcastRun {
             quic_endpoint_sender,
         )?;
         self.maybe_dispatch_mcp_shreds(
-            &shreds,
             &batch_info,
             cluster_info,
             bank_forks,
@@ -1096,7 +1095,6 @@ mod test {
             was_interrupted: false,
         });
         standard_broadcast_run.maybe_dispatch_mcp_shreds(
-            &[],
             &batch_info,
             &cluster_info,
             &bank_forks,
